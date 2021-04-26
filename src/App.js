186cb7feba474cf71697591cb6e3320629c1ab12
Tan Tano <tantn1@sunshinegroup.vn>
2021-04-26 17:01:21 +0700

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './default.scss';
 import Header from './components/Header/index';
 import HomePage from './pages/HomePage';
+import { Route } from 'react-router';
+import Registration from './pages/Registration';
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="main">
-        <HomePage />
+        <Route path="/" exact component={HomePage}></Route>
+        <Route path="/registration" component={Registration}></Route>
       </div>
     </div>
   );
---
+++ src/pages/Registration/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './styles.scss';
+
+class Registration extends Component {
+    render() {
+        return (
+            <div>
+                Registration
+            </div>
+        );
+    }
+}
+
+export default Registration;
